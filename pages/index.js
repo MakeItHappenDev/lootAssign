472748e4caf6551cdce020830914dc6e7273b8fc
PookMook <arthur.juchereau@gmail.com>
2019-10-27 22:16:51 -0400

    setup

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
 import axios from "axios";
 
-const query = `{ hello }`;
+const query = `{ 
+  bosses{
+    name
+    loots{
+      name
+      quality
+    }
+    zone{
+      name
+    }
+  }
+}`;
 
-const App = props => <h1>The response from the server is: {props.hello}</h1>;
+const App = props => 
+  <>
+    <h1>The response from the server is:</h1>
+    <pre>{JSON.stringify(props.bosses,null,1)}</pre>
+  </>;
 
 App.getInitialProps = async () => {
   const response = await axios.post(`http://localhost:4000/api/graphql`, {
